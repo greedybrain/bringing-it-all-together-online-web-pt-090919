3a8a455aeae5e62992bfaa5a323dbe58655311a1
Naya  Willis <greedybrain18@gmail.com>
2019-11-13 01:33:11 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -74,7 +74,8 @@ class Dog
   end
   
   def update 
-    sql = "UPDATE dogs "
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.name)
   end
   
 end
